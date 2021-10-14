475d5278e0c10514fbd9d60ef5f8b30bfbba740f
Salbei <salbei@gruppe-adler.de>
2021-10-14 17:00:11 +0200

    Fixed loadouts

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,7 +2,7 @@
 *   Legt allgemeine Information über die Mission fest.
 */
 
-author = "nomisum & Salbei für Gruppe Adler";                                               // Missionsersteller
+author = "B4d Horse, nomisum & Salbei für Gruppe Adler";                                               // Missionsersteller
 onLoadName = "CO Operation Five Lions";                                                   // Name der Mission
 onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\fivelions2.paa";                                                // Ladebild
