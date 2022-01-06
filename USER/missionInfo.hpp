fa4b8f0551d6e8c2e262e9ab8b3b4113f9c20f93
nomisum <nomisum@gmail.com>
2022-01-06 23:47:52 +0100

    new loading pic

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -3,8 +3,8 @@
 */
 
 author = "B4d Horse, nomisum & Salbei für Gruppe Adler";                                               // Missionsersteller
-onLoadName = "CO Operation Five Lions";                                                   // Name der Mission
+onLoadName = "CO Operation Five Lions III";                                                   // Name der Mission
 onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
-loadScreen = "data\fivelions2.paa";                                                // Ladebild
+loadScreen = "data\fivelions3.paa";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
 overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
